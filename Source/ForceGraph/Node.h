a2622794ff1c42e042c1cfe446f4b43d99490e9a
Your Name <you@example.com>
2024-08-20 19:39:16 +0800

    A

---
+++ Source/ForceGraph/Node.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+struct FNode
+{
+	FVector Position;
+	FVector Velocity;
+	FVector Force; // Optional: Direct application of force
+	bool bIsPositionFixed;
+};
---
+++ Source/ForceGraph/SimulationSystem.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Node.h"
+#include "TimerManager.h"
+#include <map>
+
+class UWorld;
+
+class SIMULATIONSYSTEM_API FSimulationSystem
+{
+public:
+    FSimulationSystem(UWorld* InWorld);
+    ~FSimulationSystem();
+
+    void Tick(float DeltaTime);
+    void InitializeNodes(int NumNodes, int NumDimensions);
+    void ApplyForces();
+    FNode* FindClosestNode(const FVector& Point, float MaxRadius);
+
+private:
+    TArray<FNode> Nodes;
+    UWorld* World;
+
+    int NumDimensions;
+    float Alpha;
+    float AlphaMin;
+    float AlphaDecay;
+    float AlphaTarget;
+    float VelocityDecay;
+
+    std::map<FString, TFunction<void(float)>> Forces; // Map of forces
+
+    void InitializeNodePositions();
+    void UpdateSimulationParameters();
+
+    FTimerHandle TimerHandle;
+}
